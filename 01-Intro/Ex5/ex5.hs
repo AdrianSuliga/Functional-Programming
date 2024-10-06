84c9a9aac8a2e36d774b5b69d1790b969aa225c2
Adrian Suliga <adriansuliga81@gmail.com>
2024-10-06 14:34:55 +0200

    Further practice else if statements

--- 01-Intro/Ex5/ex5.hs
+++ 01-Intro/Ex5/ex5.hs
@@ -1,9 +1,15 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+import Language.Haskell.TH (fromE)
 {-# HLINT ignore "Use guards" #-}
 {-# HLINT ignore "Use min" #-}
 sgn :: Int -> Int
 absInt :: Int -> Int
 min2Int :: (Int, Int) -> Int
+min3Int :: (Int, Int, Int) -> Int
+toUpper :: Char -> Char
+toLower :: Char -> Char
+isDigit :: Char -> Bool
+charToNum :: Char -> Int
 
 main :: IO ()
 
@@ -19,5 +25,15 @@ absInt n =
 min2Int (n, m) =
     if n <= m then n
     else m
+
+min3Int (x, y, z) = min2Int(x, min2Int(y, z))
+
+toUpper c = toEnum (fromEnum c - 32)
+
+toLower c = toEnum (fromEnum c + 32)
+
+isDigit c = 48 <= fromEnum c && fromEnum c <= 57 
+
+charToNum = fromEnum
 
 main = return ()
